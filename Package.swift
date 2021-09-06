90a8aaf82dab30afc4156e8d9abe810492fa5a02
Arnaud RINGENBACH <arnaud.ringenbach@niji.fr>
2021-09-06 11:05:07 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "CrossDK",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "CrossDK",
+            targets: ["CrossDK"])
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "CrossDK",
+            path: "Products/CrossDK.xcframework"
+        )
+    ]
+)
