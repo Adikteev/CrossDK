226dd264cfc1076ac7cecd9c7cc797f771ce9720
Arnaud RINGENBACH <arnaud.ringenbach@niji.fr>
2021-09-14 16:42:21 +0200

    Add technical demo

---
+++ Demo/CrossDK-demo/CrossDK-Demo/Extensions/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  CrossDK demo
+//
+//  Created by Steven VAILLE on 30/08/2021.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - UIView extention
+extension UIView {
+    /// Drops a shadow to a view.
+    func dropShadow() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.shadowRadius = 3.0
+        layer.shadowColor = #colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 1)
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.shadowOpacity = 0.4
+        layer.masksToBounds = false
+    }
+}
---
+++ Demo/CrossDK-demo/CrossDK-Demo/UIKit/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  CrossDK-demo
+//
+//  Created by Arnaud RINGENBACH on 14/09/2021.
+//
+
+import UIKit
+import CrossDK
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        CrossDKConfig.shared.setup(appId: "1206967173",
+                                   apiKey: "4da7b567a3c1402f9c78eb481ef0cfa9")
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
